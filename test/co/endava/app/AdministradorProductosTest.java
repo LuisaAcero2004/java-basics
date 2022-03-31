5edc0f37ee9956ef33f8f2a59a9aa9d6b3a08e1e
LuisaAcero2004 <luisafernanda_2004@msn.com>
2022-03-31 16:20:07 -0500

    Test satisfactorio - Validación de descuento, nombre, id y setid()

--- test/co/endava/app/AdministradorProductosTest.java
+++ test/co/endava/app/AdministradorProductosTest.java
@@ -5,6 +5,7 @@ import co.endava.datos.Producto;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.lang.Math;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,14 +15,29 @@ class AdministradorProductosTest {
     public void main() {
         double descuento;
         String nombre;
+        int id;
         nombre = "Pastel";
-        descuento = 150.0;
+        id = 103;
+        //1500 -> 100
+        //? -> 10
+        // 10*1500/100
+        descuento = 1500 * (10.0/100.0);
         Comida p3 = new Comida(103, "Pastel", 1500, LocalDate.now());
+
         //Validación descuento
         assertEquals(descuento,p3.getDescuento());
 
         //Validación nombre
         assertEquals(nombre,p3.getNombre());
+
+        //Validación id
+        assertEquals(id,p3.getId());
+
+        id = 108;
+        p3.setId(id);
+
+        //Validación set id
+        assertEquals(id,p3.getId());
 
 
     }
