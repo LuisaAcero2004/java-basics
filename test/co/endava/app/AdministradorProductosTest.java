1bec0cecf7fbeffdd8f098b4ff5bf5f6b7123943
LuisaAcero2004 <luisafernanda_2004@msn.com>
2022-03-31 15:37:19 -0500

    Test satisfactorio de descuento

--- test/co/endava/app/AdministradorProductosTest.java
+++ test/co/endava/app/AdministradorProductosTest.java
@@ -1,17 +1,24 @@
 package co.endava.app;
 
+import co.endava.datos.Comida;
 import co.endava.datos.Producto;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AdministradorProductosTest {
 
     @Test
     public void main() {
-        String nombre;
-        nombre = "Café";
-        Producto p1 = new Producto(101,"Café",15000);
-        assertEquals(nombre,p1.getNombre());
+        double descuento;
+        descuento = 200.0;
+        Comida p3 = new Comida(103, "Pastel", 2000, LocalDate.now());
+        assertEquals(descuento,p3.getDescuento());
+
+
+
     }
+
 }
